5cec57c9acbabfb03625904af48d9eb1f101f6af
Karolina Machalica <karolina.Machalica9@gmail.com>
2023-03-09 19:58:34 +0100

    Dodanie nawigacji dolnej

--- app/src/main/java/com/example/rightplace/MainActivity.kt
+++ app/src/main/java/com/example/rightplace/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.rightplace
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val navController = findNavController(R.id.fragmentContainerView)
+
+        bottomNavigationView.setupWithNavController(navController)
+
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.documentsFragment, R.id.cameraFragment, R.id.roomsFragment))
+        setupActionBarWithNavController(navController, appBarConfiguration)
     }
 }
